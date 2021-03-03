2bf31c959d66eb0546de32190273da3554042134
jdehorty <justin.dehorty@bentley.com>
2021-03-03 03:22:54 -0500

    Remove Overload bonus
    https://github.com/official-stockfish/Stockfish/commit/79e3710fd224e6e052f6bc5420a8f8f03bf41e18
    
    Bench: 3810849

--- src/evaluate.c
+++ src/evaluate.c
@@ -171,7 +171,6 @@ static const Score KingProtector      = S(  6,  7);
 static const Score KnightOnQueen      = S( 20, 12);
 static const Score LongDiagonalBishop = S( 44,  0);
 static const Score MinorBehindPawn    = S( 16,  0);
-static const Score Overload           = S( 12,  6);
 static const Score PawnlessFlank      = S( 18, 94);
 static const Score RestrictedPiece    = S(  7,  6);
 static const Score RookOnPawn         = S( 10, 28);
@@ -532,11 +531,9 @@ INLINE Score evaluate_threats(const Pos *pos, EvalInfo *ei, const int Us)
     if (weak & ei->attackedBy[Us][KING])
       score += ThreatByKing;
 
-    // Bonus for overload (non-pawn enemies attacked and defended exactly once)
-    b = weak & nonPawnEnemies & ei->attackedBy[Them][0];
-    score += Overload * popcount(b);
-
-    score += Hanging * popcount(weak & ~ei->attackedBy[Them][0]);
+    b =  ~ei->attackedBy[Them][0]
+       | (nonPawnEnemies & ei->attackedBy2[Us]);
+    score += Hanging * popcount(weak & b);
   }
 
   // Bonus for restricting their piece moves
